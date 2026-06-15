2a4e1f5b73e8d794c67302c7fa1829282781ce1e
Ayoub Abedrabbo <Ayoub.Abedrabbo@Gmail.com>
2026-06-15 12:45:56 -0400

    Fix Cloudflare Pages static deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ import mdx from '@astrojs/mdx';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://alphabravomedia.co/',
+  output: 'static',
   integrations: [
     react(),
     mdx(),
